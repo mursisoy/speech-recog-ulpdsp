a10cde5fbafef012200b6a29674252179bca8c5b
Alberto Mur <565825@unizar.es>
2016-11-27 16:27:06 +0100

    Deleted Debug files

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/Users/StFrancisco/Desktop/archivos_dsp/rts55x.lib"
-
